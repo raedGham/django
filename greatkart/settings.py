1f7fa351509154edc1d1903b97ad451cf76e6503
unknown <raed_ghamrawi@live.com>
2023-11-21 13:07:08 +0200

    store & single product

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'category',
-    'accounts'
+    'accounts',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'category.context_processors.menu_links',
             ],
         },
     },
@@ -126,5 +128,5 @@ STATICFILES_DIRS = [
 ]
 
 # media files configuration
-MEDIA_URL ='/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
