cfa92a836c6d25145a330d8f6d6a68af66103629
Raed Ghamrawi <raed_ghamrawi@live.com>
2023-11-22 17:08:11 +0200

    implementing cart functionality

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from store.models import Product 
 # Create your models here.
 
 
@@ -11,6 +11,14 @@ class Cart (models.Model):
         return self.cart_id
 
 
-class CartItem(models.Model):
+class CartItem (models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    cart = models.ForeignKey(Cart, )
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    
+    def sub_total(self):
+        return self.product.price * self.quantity
+    
+    def __str__(self):
+        return self.product
