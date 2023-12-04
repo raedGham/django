ba6aa2bb196c3a4a44ea16b028d13d1afa7204af
unknown <raed_ghamrawi@live.com>
2023-12-04 15:26:00 +0200

    rrr

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def register(request):
+    return render(request, 'accounts/register.html')
+
+
+def login(request):
+    return HttpResponse("login")
+
+
+def logout(request):
+    return HttpResponse("logout")
